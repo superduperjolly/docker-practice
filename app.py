638209b8905627c1dadadedb155e7bd2af8d5e48
superduperjolly <jolly.ann.bilad@gmail.com>
2020-05-17 17:34:01 +0800

    Adding the sample Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+"""A simple Flask app for the docker exercise"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
